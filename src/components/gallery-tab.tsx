65ec644dbaa388468d71bcd4ed313cd3a6775bb0
daveclinton <clintondavid46@gmail.com>
2024-04-09 14:29:05 +0300

    feat: add gallery tab

--- src/components/gallery-tab.tsx
+++ src/components/gallery-tab.tsx
@@ -5,22 +5,44 @@ import { Image, Text, View } from '@/ui';
 export const GalleryTab = () => {
   return (
     <View className="m-2 flex overflow-hidden  rounded-xl bg-white p-10 ">
-      <Text>Galley</Text>
-      <View className="flex flex-row  gap-x-2">
-        <Image
-          className="inline-flex h-[156px] w-1/2 items-center justify-center "
-          contentFit="cover"
-          source={{
-            uri: '',
-          }}
-        />
-        <Image
-          className="inline-flex h-[156px] w-1/2 items-center justify-center "
-          contentFit="cover"
-          source={{
-            uri: '',
-          }}
-        />
+      <Text>Gallery</Text>
+      <View className="flex flex-row flex-wrap">
+        <View className="w-1/2 p-2">
+          <Image
+            className="h-[156px] w-full rounded-lg object-cover"
+            contentFit="cover"
+            source={{
+              uri: '',
+            }}
+          />
+        </View>
+        <View className="w-1/2 p-2">
+          <Image
+            className="h-[156px] w-full rounded-lg object-cover"
+            contentFit="cover"
+            source={{
+              uri: '',
+            }}
+          />
+        </View>
+        <View className="w-1/2 p-2">
+          <Image
+            className="h-[156px] w-full rounded-lg object-cover"
+            contentFit="cover"
+            source={{
+              uri: '',
+            }}
+          />
+        </View>
+        <View className="w-1/2 p-2">
+          <Image
+            className="h-[156px] w-full rounded-lg object-cover"
+            contentFit="cover"
+            source={{
+              uri: '',
+            }}
+          />
+        </View>
       </View>
     </View>
   );
