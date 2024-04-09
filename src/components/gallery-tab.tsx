db5c54471d7403a308e695ffe5e0229cdfb3f8d6
daveclinton <clintondavid46@gmail.com>
2024-04-09 14:13:36 +0300

    feat: add gallery tab

--- src/components/gallery-tab.tsx
+++ src/components/gallery-tab.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import { Text, View } from '@/ui';
+import { Image, Text, View } from '@/ui';
 
 export const GalleryTab = () => {
   return (
-    <View>
+    <View className="m-2 flex overflow-hidden  rounded-xl bg-white p-10 ">
       <Text>Galley</Text>
+      <View className="flex flex-row  gap-x-2">
+        <Image
+          className="inline-flex h-[156px] w-1/2 items-center justify-center "
+          contentFit="cover"
+          source={{
+            uri: '',
+          }}
+        />
+        <Image
+          className="inline-flex h-[156px] w-1/2 items-center justify-center "
+          contentFit="cover"
+          source={{
+            uri: '',
+          }}
+        />
+      </View>
     </View>
   );
 };
