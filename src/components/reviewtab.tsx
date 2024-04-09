075710572b86292a936aa46d72a4b416fd0f83e1
daveclinton <clintondavid46@gmail.com>
2024-04-09 13:04:28 +0300

    feat: add reviews tabs

--- src/components/reviewtab.tsx
+++ src/components/reviewtab.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
+import { TextInput } from 'react-native';
 
-import { Text, View } from '@/ui';
+import { Button, Image, Text, View } from '@/ui';
+import { Rating } from '@/ui/icons/rating';
 
 export const ReviewTab = () => {
+  const [value, onChangeText] = React.useState('Write a review');
+  const stars = Array.from({ length: Math.floor(4.5) }, (_, index) => (
+    <Rating height={18} width={18} key={index} color="#DB1471" />
+  ));
   return (
-    <View>
-      <Text>Revies</Text>
+    <View className="m-2 flex gap-y-4 overflow-hidden  rounded-xl bg-white p-5 ">
+      <Text className="text-lg">Rate the services offered</Text>
+      <View>
+        <Rating width={24} height={24} color="#838FA0" />
+      </View>
+      <TextInput
+        editable
+        className="h-[86px] rounded-lg border border-slate-500  p-4 align-top"
+        multiline
+        maxLength={40}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+      />
+      <Button label="Submit Review" />
+      <View className="flex   pb-3">
+        <View className="flex flex-row items-center justify-between">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image
+              className="inline-flex h-20 w-20 items-center justify-center rounded-full"
+              contentFit="cover"
+              source={{
+                uri: '',
+              }}
+            />
+            <View>
+              <Text className="text-lg">Gathoni</Text>
+              <View className="flex flex-row">{stars}</View>
+            </View>
+          </View>
+          <Text className="text-lg">2 Days ago</Text>
+        </View>
+        <Text className="px-14 ">
+          Aliqua officia duis occaecat consectetur furgiat nostrud anim dolor
+          commado officia proident...
+        </Text>
+      </View>
+      <View className="flex   pb-3">
+        <View className="flex flex-row items-center justify-between">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image
+              className="inline-flex h-20 w-20 items-center justify-center rounded-full"
+              contentFit="cover"
+              source={{
+                uri: '',
+              }}
+            />
+            <View>
+              <Text className="text-lg">Gathoni</Text>
+              <View className="flex flex-row">{stars}</View>
+            </View>
+          </View>
+          <Text className="text-lg">2 Days ago</Text>
+        </View>
+        <Text className="px-14 ">
+          Aliqua officia duis occaecat consectetur furgiat nostrud anim dolor
+          commado officia proident...
+        </Text>
+      </View>
     </View>
   );
 };
--- src/ui/icons/rating.tsx
+++ src/ui/icons/rating.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import type { SvgProps } from 'react-native-svg';
 import Svg, { ClipPath, Defs, G, Path, Rect } from 'react-native-svg';
 
-export const Rating = ({ color = 'white', ...props }: SvgProps) => {
+export const Rating = ({
+  color = 'white',
+  width = 11,
+  height = 11,
+  ...props
+}: SvgProps) => {
   return (
-    <Svg width="11" height="11" viewBox="0 0 11 11" fill="none" {...props}>
+    <Svg
+      width={width}
+      height={height}
+      viewBox="0 0 11 11"
+      fill="none"
+      {...props}
+    >
       <G clipPath="url(#clip0_187_2326)">
         <Path
           d="M10.7289 3.65379L7.58884 3.17415L6.1814 0.176259C6.07628 -0.0476039 5.70899 -0.0476039 5.60387 0.176259L4.19685 3.17415L1.05681 3.65379C0.798901 3.69338 0.695907 4.00704 0.876785 4.19217L3.15797 6.53039L2.61875 9.83599C2.57576 10.0986 2.85623 10.2956 3.08818 10.1667L5.89285 8.61666L8.69752 10.1671C8.92734 10.2948 9.21036 10.1016 9.16695 9.83641L8.62772 6.53082L10.9089 4.1926C11.0898 4.00704 10.9864 3.69338 10.7289 3.65379Z"
