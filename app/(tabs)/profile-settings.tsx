68224af3920b329e66c04f6a433e36e8c464d6a2
David Clinton <clinton@192.168.0.106>
2024-07-03 11:55:28 +0300

    feat: add inot fields

--- app/(tabs)/profile-settings.tsx
+++ app/(tabs)/profile-settings.tsx
@@ -1,9 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
+import CustomButton from "@/components/Button";
+import { FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TextInput as PaperInput, TextInput } from "react-native-paper";
+
+const CustomInput = ({
+  label,
+  text,
+  onChange,
+  rightIcon,
+}: {
+  label: string;
+  text: string;
+  onChange: () => void;
+  rightIcon?: React.ReactNode;
+}) => {
+  return (
+    <PaperInput
+      label={label}
+      mode="outlined"
+      textColor="black"
+      activeOutlineColor="#DB1471"
+      outlineStyle={{
+        borderColor: "#D9D9D9",
+      }}
+      value={text}
+      onChangeText={onChange}
+      right={rightIcon}
+    />
+  );
+};
 
 const ProfileSettings = () => {
+  const [text, setText] = React.useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Profile Settings</Text>
+      <View style={styles.formContainer}>
+        <CustomInput label="Name" text={text} onChange={() => setText(text)} />
+        <CustomInput label="Email" text={text} onChange={() => setText(text)} />
+        <CustomInput
+          label="Birthday"
+          text={text}
+          onChange={() => setText(text)}
+          rightIcon={<TextInput.Icon icon={"calendar"} color="#D9D9D9" />}
+        />
+        <CustomInput
+          label="Phone number"
+          text={text}
+          onChange={() => setText(text)}
+        />
+        <CustomButton buttonText="Save Changes" />
+      </View>
     </View>
   );
 };
@@ -11,6 +60,7 @@ const ProfileSettings = () => {
 const styles = StyleSheet.create({
   container: { padding: 20, backgroundColor: "#F6F6F9", gap: 20 },
   header: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
+  formContainer: { gap: 20 },
 });
 
 export default ProfileSettings;
--- hooks/format-time-stamp.ts
+++ hooks/format-time-stamp.ts
@@ -3,13 +3,3 @@ type TimeObj = {
   minutes: number;
   seconds: number;
 };
-
-const convertToTimestamp = (timeObj: TimeObj): number => {
-  const { hours, minutes, seconds } = timeObj;
-  const date = new Date();
-  date.setHours(hours);
-  date.setMinutes(minutes);
-  date.setSeconds(seconds);
-  date.setMilliseconds(0);
-  return date.getTime();
-};
