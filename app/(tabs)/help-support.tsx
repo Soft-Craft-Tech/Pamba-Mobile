2a8d887e50c0abe21e177d0ae109245f02322e12
David Clinton <clinton@192.168.0.106>
2024-07-03 14:05:28 +0300

    feat: add help suppot

--- app/(tabs)/help-support.tsx
+++ app/(tabs)/help-support.tsx
@@ -1,9 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import CustomButton from "@/components/Button";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const HelpandSupport = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Help and Support</Text>
+      <View style={styles.formContainer}>
+        <Text>Report a problem</Text>
+        <Text>
+          Did you encounter a problem? Please tell us where the problem is so
+          that we can fix the issue fast
+        </Text>
+        <TextInput style={styles.input} placeholder="Notes" multiline />
+        <CustomButton buttonText="Save Changes" />
+      </View>
     </View>
   );
 };
@@ -11,6 +21,15 @@ const HelpandSupport = () => {
 const styles = StyleSheet.create({
   container: { padding: 20, backgroundColor: "#F6F6F9", gap: 20 },
   header: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
+  formContainer: { gap: 20 },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 16,
+    minHeight: 100,
+  },
 });
 
 export default HelpandSupport;
