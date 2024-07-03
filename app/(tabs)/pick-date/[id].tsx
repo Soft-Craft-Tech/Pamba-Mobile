32d687b991c89303a60aa972900c1abdfe07a8ff
David Clinton <clinton@192.168.0.106>
2024-07-03 13:46:01 +0300

    feat: add date selection

--- app/(tabs)/pick-date/[id].tsx
+++ app/(tabs)/pick-date/[id].tsx
@@ -1,77 +1,108 @@
-import { LinearGradient } from "expo-linear-gradient";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   ScrollView,
   StyleSheet,
-  Modal,
 } from "react-native";
-import { TimerPickerModal } from "react-native-timer-picker";
-import * as Haptics from "expo-haptics";
 import { Dropdown } from "react-native-element-dropdown";
 import { Avatar } from "react-native-paper";
 import CustomButton from "@/components/Button";
 import { FontAwesome5 } from "@expo/vector-icons";
-import DatePicker from "react-native-modern-datepicker";
+import { DatePickerModal, TimePickerModal } from "react-native-paper-dates";
 import { useRouter } from "expo-router";
+import { en, registerTranslation } from "react-native-paper-dates";
+import { format } from "date-fns";
+
+registerTranslation("en", en);
+
 type TimeObj = {
   hours: number;
   minutes: number;
-  seconds: number;
 };
 
-const data = [
-  { label: "Item 1", value: "1" },
-  { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
+type DayInfo = {
+  day: string;
+  date: string;
+  slots: number;
+  fullDate: Date; // Add fullDate property to the DayInfo type
+};
+
+type ProviderData = {
+  label: string;
+  value: string;
+  avatar: string;
+};
+
+const providerData: ProviderData[] = [
+  {
+    label: "Olivia Rhae",
+    value: "1",
+    avatar: "https://i.pravatar.cc/150?img=27",
+  },
+  { label: "John Doe", value: "2", avatar: "https://i.pravatar.cc/150?img=28" },
 ];
 
-const convertTo24HourFormat = (timeObj: TimeObj): string => {
-  const { hours, minutes } = timeObj;
-  const formattedHours = String(hours).padStart(2, "0");
-  const formattedMinutes = String(minutes).padStart(2, "0");
-  return `${formattedHours}:${formattedMinutes}`;
-};
+const days: DayInfo[] = Array.from({ length: 7 }, (_, index) => {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + index);
+  const day = currentDate.toLocaleString("en-US", { weekday: "short" });
+  const date = currentDate.toLocaleString("en-US", {
+    day: "2-digit",
+    month: "short",
+  });
+  const slots = Math.floor(Math.random() * 10);
+  return { day, date, slots, fullDate: currentDate };
+});
 
-const PickDate = () => {
+const PickDate: React.FC = () => {
   const router = useRouter();
-  const [selectedDay, setSelectedDay] = useState("Fri");
-  const [showPicker, setShowPicker] = useState(false);
-  const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
+  const [selectedDay, setSelectedDay] = useState<string>(days[0]?.day);
 
-  const [selectedTime, setSelectedTime] = useState("Select Time");
-  const [openCalendar, setOpenCalendar] = useState(false);
-  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedProvider, setSelectedProvider] = useState<string | null>(null);
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [date, setDate] = useState<any | undefined>();
+  const [open, setOpen] = useState<boolean>(false);
+  const [selectedTime, setSelectedTime] = useState<TimeObj | null>(null);
+  const [visible, setVisible] = useState<boolean>(false);
 
-  const days = [
-    { day: "Fri", date: "03 Feb", slots: 16 },
-    { day: "Sun", date: "03 Feb", slots: 2 },
-    { day: "Mon", date: "03 Feb", slots: 2 },
-    { day: "Tue", date: "03 Feb", slots: 5 },
-    { day: "Wed", date: "03 Feb", slots: 5 },
-    { day: "Thur", date: "03 Feb", slots: 5 },
-  ];
+  const onDismissTime = useCallback(() => setVisible(false), []);
+  const onConfirmTime = useCallback(({ hours, minutes }: TimeObj) => {
+    setVisible(false);
+    setSelectedTime({ hours, minutes });
+  }, []);
+
+  const onDismissDate = useCallback(() => setOpen(false), []);
+  const onConfirmDate = useCallback(({ date }: { date: any }) => {
+    setOpen(false);
+    setDate(date);
+  }, []);
+
+  const formatTime = (time: TimeObj | null): string => {
+    if (!time) return "Select Time";
+    const { hours, minutes } = time;
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+      2,
+      "0"
+    )} HRS`;
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Select a day</Text>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        {days.map((item, index) => (
+        {days.map((item: DayInfo) => (
           <TouchableOpacity
-            key={index}
+            key={item.day}
             style={[
               styles.dayButton,
               selectedDay === item.day && styles.selectedDay,
             ]}
-            onPress={() => setSelectedDay(item.day)}
+            onPress={() => {
+              setSelectedDay(item.day);
+              setDate(item.fullDate.toDateString());
+            }}
           >
             <Text style={styles.dayText}>{item.day}</Text>
             <Text style={styles.dateText}>{item.date}</Text>
@@ -79,113 +110,80 @@ const PickDate = () => {
           </TouchableOpacity>
         ))}
       </ScrollView>
-      {selectedDate !== "" && (
-        <View style={styles.selectedDate}>
-          <Text>Selected Date: {selectedDate}</Text>
-        </View>
+      {date && (
+        <Text style={styles.dateSelected}>
+          Selected Date: {format(date, "do MMMM")}
+        </Text>
       )}
-      <TouchableOpacity onPress={() => setShowPicker(true)}>
-        <TimerPickerModal
-          visible={showPicker}
-          hourLabel=":"
-          minuteLabel=""
-          hideSeconds
-          setIsVisible={setShowPicker}
-          onConfirm={(pickedDuration) => {
-            setSelectedTime(convertTo24HourFormat(pickedDuration));
-            setShowPicker(false);
-          }}
-          modalTitle="Select Time"
-          onCancel={() => setShowPicker(false)}
-          closeOnOverlayPress
-          LinearGradient={LinearGradient}
-          Haptics={Haptics}
-          styles={{
-            theme: "light",
-          }}
-          modalProps={{
-            overlayOpacity: 0.2,
-          }}
-        />
-        <Text style={styles.subHeader}>Choose a slot for your haircut</Text>
-
-        <View style={styles.timeInput}>
-          <Text>
-            {`${selectedTime} ${selectedTime !== "Select Time" ? "HRS" : ""}`}
-          </Text>
-        </View>
+      <Text style={styles.subHeader}>Choose a slot for your haircut</Text>
+      <TouchableOpacity
+        onPress={() => setVisible(true)}
+        style={styles.timeInput}
+      >
+        <Text>{formatTime(selectedTime)}</Text>
       </TouchableOpacity>
+
       <Text style={styles.subHeader}>Select service provider</Text>
-      <View style={styles.providerContainer}>
-        <Dropdown
-          style={[styles.dropdown, isFocus && { borderColor: "#DB1471" }]}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={data}
-          search
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          placeholder={!isFocus ? "Olivia Rhae (Optional)" : "..."}
-          searchPlaceholder="Search..."
-          value={value}
-          onFocus={() => setIsFocus(true)}
-          onBlur={() => setIsFocus(false)}
-          onChange={(item) => {
-            setValue(item.value as any);
-            setIsFocus(false);
-          }}
-          renderLeftIcon={() => (
-            <Avatar.Image
-              size={30}
-              source={{ uri: "https://i.pravatar.cc/150?img=27" }}
-            />
-          )}
-        />
-      </View>
+      <Dropdown<ProviderData>
+        style={[styles.dropdown, isFocus && { borderColor: "#DB1471" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={providerData}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? "Select Provider (Optional)" : "..."}
+        searchPlaceholder="Search..."
+        value={selectedProvider}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setSelectedProvider(item.value);
+          setIsFocus(false);
+        }}
+        renderLeftIcon={(item) => (
+          <Avatar.Image
+            size={30}
+            source={{ uri: "https://i.pravatar.cc/150" }}
+          />
+        )}
+      />
+
       <View style={styles.calendarSelect}>
         <Text style={styles.different}>Need a different day?</Text>
         <TouchableOpacity
-          onPress={() => {
-            setOpenCalendar(true);
-          }}
+          onPress={() => setOpen(true)}
           style={styles.leftSection}
         >
           <FontAwesome5 name="calendar-check" size={24} color="#007B99" />
           <Text style={styles.selectText}>SELECT DATE</Text>
         </TouchableOpacity>
       </View>
-      <Modal animationType="fade" transparent={true} visible={openCalendar}>
-        <View style={styles.centerdView}>
-          <View style={styles.modalView}>
-            <DatePicker
-              options={{
-                backgroundColor: "#FFFFFF",
-                textHeaderColor: "#1C1C1C",
-                textDefaultColor: "#1C1C1C",
-                selectedTextColor: "#DB1471",
-                mainColor: "#1C1C1C",
-                textSecondaryColor: "#DB1471",
-                borderColor: "#1C1C1C",
-              }}
-              mode="datepicker"
-              style={{ borderRadius: 10, borderWidth: 0 }}
-              onDateChange={(selectedDate) => {
-                setSelectedDate(selectedDate);
-                setOpenCalendar(false);
-              }}
-            />
-          </View>
-        </View>
-      </Modal>
 
       <CustomButton
-        onPress={() => {
-          router.push(`/confirm-appointment/${23}`);
-        }}
+        onPress={() => router.push(`/confirm-appointment/${23}`)}
         buttonText="Book Appointment"
+      />
+
+      <TimePickerModal
+        visible={visible}
+        onDismiss={onDismissTime}
+        onConfirm={onConfirmTime}
+        hours={selectedTime?.hours || 12}
+        minutes={selectedTime?.minutes || 0}
+        animationType="fade"
+      />
+
+      <DatePickerModal
+        locale="en"
+        mode="single"
+        visible={open}
+        onDismiss={onDismissDate}
+        date={date}
+        onConfirm={onConfirmDate}
       />
     </View>
   );
@@ -214,23 +212,6 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 10,
   },
-  timeSlotContainer: { flexDirection: "row", flexWrap: "wrap" },
-  timeSlot: { padding: 10, borderWidth: 1, borderRadius: 5, margin: 5 },
-  selectedTime: { backgroundColor: "lightblue" },
-  providerContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  providerImage: { width: 40, height: 40, borderRadius: 20, marginRight: 10 },
-  bookButton: {
-    backgroundColor: "pink",
-    padding: 15,
-    borderRadius: 5,
-    alignItems: "center",
-    marginTop: 20,
-  },
-  bookButtonText: { color: "white", fontWeight: "bold" },
   timeInput: {
     width: "100%",
     padding: 15,
@@ -249,65 +230,24 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#fff",
   },
-  icon: {
-    marginRight: 5,
-  },
-  label: {
-    position: "absolute",
-    backgroundColor: "white",
-    left: 22,
-    top: 8,
-    zIndex: 999,
-    paddingHorizontal: 8,
-    fontSize: 14,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-    marginLeft: 20,
-  },
-  selectedTextStyle: {
-    fontSize: 16,
-    marginLeft: 20,
-  },
-  iconStyle: {
-    width: 20,
-    height: 20,
-  },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
+  placeholderStyle: { fontSize: 16, marginLeft: 20 },
+  selectedTextStyle: { fontSize: 16, marginLeft: 20 },
+  iconStyle: { width: 20, height: 20 },
+  inputSearchStyle: { height: 40, fontSize: 16 },
   calendarSelect: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: 20,
+    marginVertical: 20,
   },
   leftSection: {
     flexDirection: "row",
+    alignItems: "center",
     gap: 3,
-    alignItems: "center",
   },
-  selectText: {
-    color: "#007B99",
-  },
-  different: {
-    fontSize: 14,
-    fontWeight: "600",
-  },
-  centerdView: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  modalView: {
-    marginHorizontal: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 8,
-  },
-  selectedDate: {
-    marginTop: 10,
-  },
+  selectText: { color: "#007B99" },
+  different: { fontSize: 14, fontWeight: "600" },
+  dateSelected: { marginTop: 20 },
 });
 
 export default PickDate;
