59f6bb45a66229315ec57c5cf1a8d3467415a5af
David Clinton <clinton@192.168.0.106>
2024-07-03 11:15:01 +0300

    feat: add profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,120 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  AntDesign,
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Profile = () => {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Settings</Text>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/profile-settings");
+          }}
+          style={styles.itemWrapper}
+        >
+          <View style={styles.itemLeft}>
+            <Feather name="user" size={24} color="#667085" />
+            <Text>Profile Settings</Text>
+          </View>
+          <View>
+            <FontAwesome5 name="chevron-right" size={20} color="#667085" />
+          </View>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/password-security");
+          }}
+          style={styles.itemWrapper}
+        >
+          <View style={styles.itemLeft}>
+            <Feather name="lock" size={20} color="#667085" />
+            <Text>Password & Security</Text>
+          </View>
+          <View>
+            <FontAwesome5 name="chevron-right" size={20} color="#667085" />
+          </View>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/help-support");
+          }}
+          style={styles.itemWrapper}
+        >
+          <View style={styles.itemLeft}>
+            <FontAwesome name="question-circle-o" size={20} color="#667085" />
+            <Text>Help and Support</Text>
+          </View>
+          <View>
+            <FontAwesome5 name="chevron-right" size={20} color="#667085" />
+          </View>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => {
+            router.push(`/privacy-policy/${1}`);
+          }}
+          style={styles.itemWrapper}
+        >
+          <View style={styles.itemLeft}>
+            <MaterialIcons name="privacy-tip" size={20} color="#667085" />
+            <Text>Privacy and Safety</Text>
+          </View>
+          <View>
+            <FontAwesome5 name="chevron-right" size={20} color="#667085" />
+          </View>
+        </TouchableOpacity>
+        <View style={styles.divider} />
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/login");
+          }}
+          style={styles.itemWrapper}
+        >
+          <View style={styles.itemLeft}>
+            <AntDesign name="logout" size={24} color="#667085" />
+            <Text>Logout of Account</Text>
+          </View>
+          <View>
+            <FontAwesome5 name="chevron-right" size={20} color="#667085" />
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { padding: 20, backgroundColor: "#F6F6F9", gap: 20 },
+  container: { flex: 1, padding: 20, backgroundColor: "#F6F6F9", gap: 20 },
   header: { fontSize: 18, fontWeight: "bold", marginBottom: 10 },
+  profileContainer: {
+    backgroundColor: "#fff",
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  itemWrapper: { flexDirection: "row", justifyContent: "space-between" },
+  itemLeft: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 5,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#ddd",
+    marginTop: 20,
+    marginBottom: 20,
+  },
 });
 
 export default Profile;
